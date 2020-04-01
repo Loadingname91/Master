60975fee46763e85c2768286c1247b89a013d8c7
Hitesh <gopinathhitesh@gmail.com>
2020-04-01 21:43:21 +0530

    Git hub integration

--- test.py
+++ test.py
@@ -1,5 +1,23 @@
 from flask import json
 from flask import request
+from flask import Flask
 
-print("hello")
-print("hello")1
+app = Flask(__name__)
+
+
+@app.route('/')
+def api_root():
+    return "welcome guys"
+
+
+@app.route('/github', methods=['POST'])
+def github():
+    if request.headers["Content-Type"] == "application/json":
+        info1 = json.dumps(request.json)
+        info = json.loads(info1)
+        print(info["label"])
+        return info1
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
